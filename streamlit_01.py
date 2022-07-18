dc66038ce49306a0b4005f7697ca061c952aabf2
cyhsieh0119 <96895088+cyhsieh0119@users.noreply.github.com>
2022-07-18 16:56:12 +0800

    Update streamlit_01.py

--- streamlit_01.py
+++ streamlit_01.py
@@ -7,12 +7,14 @@ st.markdown("# Patent Search page 🎈")
 st.sidebar.markdown("# Main page 🎈 of Side Bar")
 
 st.write(st.__version__)
-Corp = st.text_input('Please input the keyword of company :', 'streamlit')
+Corp = st.text_input('Please input the keyword of company :', 'apple')
+
 link2='https://patents.google.com/xhr/query?url=assignee%3D{}%26country%3DUS%26language%3DENGLISH%26type%3DPATENT%26num%3D100%26sort%3Dnew&exp=&download=true'.format(Corp)
-tt=pd.read_csv(link2,skiprows=1)
+
+patent_data=pd.read_csv(link2,skiprows=1)
 option = st.selectbox(
      'Please choose the item：',
      ('priority date', 'filing/creation date', 'publication date'))
 
-t1=tt.sort_values(by=option,ascending=False)
-st.write(t1)
+df=patent_data.sort_values(by=option,ascending=False)
+st.write(df)
