135e199d0d024815b0d40b07e0ff515a8cc26e38
cyhsieh0119 <96895088+cyhsieh0119@users.noreply.github.com>
2022-07-18 18:40:34 +0800

    Update streamlit_01.py

--- streamlit_01.py
+++ streamlit_01.py
@@ -10,25 +10,27 @@ st.set_page_config(page_title="Patent Search"
 st.markdown("# Patent Search page 🎈")
 st.sidebar.markdown("# Main page 🎈 of Side Bar")
 
+
 st.write(st.__version__)
 Corp = st.text_input('Please input the keyword of company :', 'apple')
-
 txt=Corp.split(' ')
-#len(test)
+#
+#
 if len(txt) >2:
   st.error('Type Error!! Company name need to < 3 ')
-elif len(txt)==1:
-  link2='https://patents.google.com/xhr/query?url=assignee%3D{}%26country%3DUS%26language%3DENGLISH%26type%3DPATENT%26num%3D100%26sort%3Dnew&exp=&download=true'.format(txt[0])
-  patent_data=pd.read_csv(link2,skiprows=1)
 else :
-  link2='https://patents.google.com/xhr/query?url=assignee%3D{}%2B{}%26country%3DUS%26language%3DENGLISH%26type%3DPATENT%26num%3D100%26sort%3Dnew&exp=&download=true'.format(txt[0],txt[1])
-  patent_data=pd.read_csv(link2,skiprows=1)
-#patent_data=pd.read_csv(link2,skiprows=1)
-option = st.selectbox(
+  if len(txt)==1:
+    link2='https://patents.google.com/xhr/query?url=assignee%3D{}%26country%3DUS%26language%3DENGLISH%26type%3DPATENT%26num%3D100%26sort%3Dnew&exp=&download=true'.format(txt[0])
+    patent_data=pd.read_csv(link2,skiprows=1)
+  else :
+    link2='https://patents.google.com/xhr/query?url=assignee%3D{}%2B{}%26country%3DUS%26language%3DENGLISH%26type%3DPATENT%26num%3D100%26sort%3Dnew&exp=&download=true'.format(txt[0],txt[1])
+    patent_data=pd.read_csv(link2,skiprows=1)
+  #
+  option = st.selectbox(
      'Please choose the item：',
      ('priority date', 'filing/creation date', 'publication date'))
-
-columns1=['id', 'title', 'priority date', 'filing/creation date', 'publication date', 'grant date', 'inventor/author',  'assignee']
-new_df=patent_data[columns1]
-df=new_df.sort_values(by=option,ascending=False).set_index(option)
-st.write(df)
+  #
+  columns1=['id', 'title', 'priority date', 'filing/creation date', 'publication date', 'grant date', 'inventor/author',  'assignee']
+  new_df=patent_data[columns1]
+  df=new_df.sort_values(by=option,ascending=False).set_index(option)
+  st.write(df)
