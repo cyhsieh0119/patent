fe3d11adf6a4d72bac7afe1d061173b9492faaba
cyhsieh0119 <96895088+cyhsieh0119@users.noreply.github.com>
2022-07-18 16:22:50 +0800

    Update streamlit_01.py

--- streamlit_01.py
+++ streamlit_01.py
@@ -1,23 +1,4 @@
-import json
-import time
-import random
-import datetime
-
-import requests
-import numpy as np
-import pandas as pd
 import streamlit as st
-import streamlit.components.v1 as components
-#from streamlit_echarts import st_echarts
-
-from streamlit.server.server import Server
-from streamlit.scriptrunner import get_script_run_ctx as get_report_ctx
 
 st.markdown("# Main page 🎈")
 st.sidebar.markdown("# Main page 🎈")
-
-Corp='Kymeta'
-link2='https://patents.google.com/xhr/query?url=assignee%3D{}%26country%3DUS%26language%3DENGLISH%26type%3DPATENT%26num%3D100%26sort%3Dnew&exp=&download=true'.format(Corp)
-tt=pd.read_csv(link2,skiprows=1)
-tt.shape
-#print(tt.columns)
