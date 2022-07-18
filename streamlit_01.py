6160db392279a9faceb0a32459530e93b982a84e
cyhsieh0119 <96895088+cyhsieh0119@users.noreply.github.com>
2022-07-18 16:28:49 +0800

    Update streamlit_01.py

--- streamlit_01.py
+++ streamlit_01.py
@@ -1,4 +1,7 @@
 import streamlit as st
 
 st.markdown("# Main page 🎈")
-st.sidebar.markdown("# Main page 🎈")
+st.sidebar.markdown("# Main page 🎈 of Side Bar")
+st.set_page_config(page_icon="📈", page_title="Patent Search")
+
+st.write(st.__version__)
