fa305efc4934930fc0a96eb4843f1e3587f88c1b
cyhsieh0119 <96895088+cyhsieh0119@users.noreply.github.com>
2022-07-18 16:18:31 +0800

    Update streamlit_01.py

--- streamlit_01.py
+++ streamlit_01.py
@@ -8,21 +8,17 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 import streamlit.components.v1 as components
+ from streamlit_echarts import st_echarts
 from streamlit_echarts import st_echarts
 
 from streamlit.server.server import Server
-# from streamlit.script_run_context import get_script_run_ctx as get_report_ctx
 from streamlit.scriptrunner import get_script_run_ctx as get_report_ctx
 
-import graphviz
-import pydeck as pdk
-import altair as alt
-import plotly.figure_factory as ff
-import matplotlib.pyplot as plt
-from pyecharts.charts import *
-from pyecharts.globals import ThemeType
-from pyecharts import options as opts
-from pyecharts.commons.utils import JsCode
+st.markdown("# Main page 🎈")
+st.sidebar.markdown("# Main page 🎈")
 
-from PIL import Image
-from io import BytesIO
+Corp='Kymeta'
+link2='https://patents.google.com/xhr/query?url=assignee%3D{}%26country%3DUS%26language%3DENGLISH%26type%3DPATENT%26num%3D100%26sort%3Dnew&exp=&download=true'.format(Corp)
+tt=pd.read_csv(link2,skiprows=1)
+tt.shape
+#print(tt.columns)
